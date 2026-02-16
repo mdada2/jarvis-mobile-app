261671a471b08577c4d4a4e587745dbd27d9700e
mdada2 <96047484+mdada2@users.noreply.github.com>
2026-02-16 16:00:13 +0530

    Delete screens directory

--- screens/__init__.py
+++
@@ -1,6 +0,0 @@
-# Screens package
-from .chat_screen import ChatScreen
-from .settings_screen import SettingsScreen
-from .history_screen import HistoryScreen
-
-__all__ = ['ChatScreen', 'SettingsScreen', 'HistoryScreen']
